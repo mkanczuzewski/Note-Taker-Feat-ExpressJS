f8f0876b68bbe8d85ed4ba06e83dae367414fcc4
mkanczuzewski <mkanczuzewski@gmail.com>
2021-12-18 13:40:39 -0500

    Add in ID get route

--- server.js
+++ server.js
@@ -18,12 +18,32 @@ function filterByQuery(query, notesArray)
     return filteredResults;
 };
 
-app.get('/api/notes', (req, res) => {
+function findById(id, notesArray)
+{
+    const result = notesArray.filter(notes => notes.id === id)[0];
+    return result;
+}
+
+app.get('/api/notes', (req, res) => 
+{
     let results = notes;
     if (req.query) {
         results = filterByQuery(req.query, results);
     }
     res.json(results);
+});
+
+app.get('/api/notes/:id', (req, res) => 
+{
+    const result = findById(req.params.id, notes);
+    if (result) 
+    {
+        res.json(result);
+    }
+    else
+    {
+        res.send(404);
+    }
 });
 
 app.listen(PORT, () => {
